ae31c494dd60fef62a6a46cdb598101007528e48
Jacob Calleon <jacob@calleon.one>
2025-11-03 15:35:53 -0800

    point cloud backend to HCP workspace Terriform-Azure

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
   cloud {
     organization = "jacobia"
     workspaces {
-      name = "learn-terraform-azure"
+      name = "Terriform-Azure"
     }
   }
 }
@@ -19,11 +19,6 @@ terraform {
 provider "azurerm" {
   features {}
   skip_provider_registration = true
-
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
 }
 
 resource "azurerm_resource_group" "rg" {
