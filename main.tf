43087f5f7974d093efc67be646b14e2b531c0c3e
Jacob Calleon <jacob@calleon.one>
2025-11-04 19:53:22 -0800

    update main.tf to deploy CIS Windows Server 2025 L2 Gen2

--- main.tf
+++ main.tf
@@ -1,38 +1,111 @@
-terraform {
-  required_version = ">= 1.1.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-
-  cloud {
-    organization = "jacobia"
-    workspaces {
-      name = "Terraform-Azure"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
   skip_provider_registration = true
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-
-  tags = var.tags
+  name     = "rg-cis-ws2025"
+  location = "westus2"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "myTFVnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = var.location
+  name                = "cis2025-vnet"
+  address_space       = ["10.42.0.0/16"]
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+}
 
-  tags = var.tags
+resource "azurerm_subnet" "subnet" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.42.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "cis2025-nsg"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  security_rule {
+    name                       = "Allow-RDP"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefixes    = ["YOUR.PUBLIC.IP/32"]
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "cis2025-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "cis2025-nic"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Required: accept the Marketplace plan before deploying
+resource "azurerm_marketplace_agreement" "cis_terms" {
+  publisher = "center-for-internet-security-inc"
+  offer     = "cis-windows-server"
+  plan      = "cis-windows-server2025-l2-gen2"
+}
+
+resource "azurerm_windows_virtual_machine" "cis_vm" {
+  name                = "cis-ws2025-l2"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2s"       # cost-effective lab size
+  admin_username      = "azureuser"
+  admin_password      = "P@ssword123!"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "center-for-internet-security-inc"
+    offer     = "cis-windows-server"
+    sku       = "cis-windows-server2025-l2-gen2"
+    version   = "latest"
+  }
+
+  plan {
+    name      = "cis-windows-server2025-l2-gen2"
+    product   = "cis-windows-server"
+    publisher = "center-for-internet-security-inc"
+  }
+
+  enable_automatic_updates = false
+  patch_mode               = "Manual"
+
+  depends_on = [azurerm_marketplace_agreement.cis_terms]
+}
+
+output "public_ip" {
+  value = azurerm_public_ip.pip.ip_address
 }
